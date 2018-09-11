1798119de3294f5330a4eef28639b8b14ffb5b8c
devinqw13 <38698098+devinqw13@users.noreply.github.com>
2018-09-11 13:34:06 -0400

    Add files via upload

---
+++ Invoice Total/Invoice Total/frmInvoiceTotal.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Invoice_Total
+{
+    public partial class frmInvoiceTotal : Form
+    {
+        public frmInvoiceTotal()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSubtotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal subtotal = Convert.ToDecimal(txtSubtotal.Text);
+            decimal discountPercent = .3m;
+            decimal discountAmount = subtotal * discountPercent;
+            decimal invoiceTotal = subtotal - discountAmount;
+
+
+            txtDiscountPercent.Text = discountPercent.ToString("p2");
+            txtDiscountAmount.Text = discountAmount.ToString("c1");
+            txtTotal.Text = invoiceTotal.ToString("c"); 
+
+
+            txtSubtotal.Focus();
+
+          
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
